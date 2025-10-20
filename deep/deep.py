2b8182247a57266c605c05fdab78f5281c77d25f
ProfToosie <marco.castro.2303@gmail.com>
2025-10-20 12:52:08 -0300

    adding one line of code

--- deep/deep.py
+++ deep/deep.py
@@ -1,5 +1,6 @@
 def main():
     resp = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+    resp = resp.strip().lower()
     if resp == "42" or resp == "forty-two" or resp == "forty two":
         print("Yes")
     else:
