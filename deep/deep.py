ed8f8f10738cf6fabc393abbc65c9dbd973d28ed
ProfToosie <marco.castro.2303@gmail.com>
2025-10-20 15:14:10 +0000

    Mon, Oct 20, 2025, 12:14 PM -03:00

---
+++ deep/deep.py
@@ -0,0 +1,8 @@
+def main():
+    resp = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+    if resp == "42" or resp == "forty-two" or resp == "forty two":
+        print("Yes")
+    else:
+        print("No")
+
+main()
