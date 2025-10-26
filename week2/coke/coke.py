02179ddbcb6b943c745f74269d0d5143de77ae93
ProfToosie <marco.castro.2303@gmail.com>
2025-10-26 16:36:56 -0300

    Vendo Coca a 50

--- week2/coke/coke.py
+++ week2/coke/coke.py
@@ -1,14 +1,25 @@
 def main():
     cokita = 50
     monea = 0
-    monto = int(input("Inserte una moneda: "))
-    if monto == 5:
-        monea = monea + 5
-    if monto == 10:
-        monea = monea + 10
-    if monto == 25:
-        monea = monea + 25
-    while monea < 50:
-        print("falta plata po ctm")
-        break
+
+    while monea < cokita:
+        monto = int(input("Ingrese moneda "))
+        if monto == 5:
+            monea = monea + 5
+            print(f"Faltan: {cokita - monea}", end="\n")
+        elif monto == 10:
+            monea = monea + 10
+            print(f"Faltan: {cokita - monea}", end="\n")
+        elif monto == 25:
+            monea = monea + 25
+            print(f"Faltan: {cokita - monea}", end="\n")
+        elif monea == cokita:
+            break
+        else:
+            print(f"MONEDA FALSA !! LLAMAR A CARABINEROS !!", end="\n")
+            break
+
+    if (cokita - monea) <= 0:
+        print(f"Change Owed: {monea - cokita}", end="\n")
+
 main()
